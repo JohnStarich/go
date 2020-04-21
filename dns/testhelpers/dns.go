0417938ff85ad2c617196083337690c5cf6c97a1
John Starich <johnstarich@johnstarich.com>
2020-04-20 23:09:51 -0500

    Add DNS testhelper

---
+++ dns/testhelpers/dns.go
@@ -0,0 +1,71 @@
+package testhelpers
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/miekg/dns"
+	"github.com/stretchr/testify/require"
+)
+
+const dom = "dns.local."
+
+func hostnamesHandler(t *testing.T, ctx context.Context, responseDelay time.Duration, hostnames map[string][]string) dns.HandlerFunc {
+	return func(w dns.ResponseWriter, r *dns.Msg) {
+		timer := time.NewTimer(responseDelay)
+		defer timer.Stop()
+		select {
+		case <-timer.C:
+		case <-ctx.Done():
+			return
+		}
+
+		var message dns.Msg
+		message.SetReply(r)
+
+		switch r.Question[0].Qtype {
+		case dns.TypeA:
+			hostname := r.Question[0].Name
+			for _, ip := range hostnames[hostname] {
+				message.Answer = append(message.Answer, &dns.A{
+					Hdr: dns.RR_Header{Name: dom, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+					A:   net.ParseIP(ip).To4(),
+				})
+			}
+		}
+
+		t.Logf("DNS message:\n%s", message.String())
+		w.WriteMsg(&message)
+	}
+}
+
+type DNSConfig struct {
+	ResponseDelay time.Duration
+	Hostnames     map[string][]string
+	Port          int
+}
+
+func StartDNSServer(t *testing.T, config DNSConfig) (address string, cancel context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	mux := dns.NewServeMux()
+	mux.HandleFunc("local.", hostnamesHandler(t, ctx, config.ResponseDelay, config.Hostnames))
+
+	packetConn, err := net.ListenUDP("udp", &net.UDPAddr{Port: config.Port})
+	require.NoError(t, err)
+	server := &dns.Server{
+		Net:        "udp",
+		PacketConn: packetConn,
+		Handler:    mux,
+	}
+	go func() {
+		err := server.ActivateAndServe()
+		require.NoError(t, err)
+	}()
+	go func() {
+		<-ctx.Done()
+		_ = server.Shutdown()
+	}()
+	return server.PacketConn.LocalAddr().String(), cancel
+}
