491433018a6b182d89c243dd5fe9c9e2ff60e39a
John Starich <johnstarich@johnstarich.com>
2020-05-18 01:18:26 -0500

    Add usage and installation godocs

--- gopages/doc.go
+++ gopages/doc.go
@@ -1,3 +1,15 @@
 // gopages generates static files for Go documentation, formatted with godoc.
 //
+// Installation:
+//   go get github.com/johnstarich/go/gopages
+//
+// Generate documentation for your module by running without any flags.
+//
+// A 'go.mod' file must be present in the current directory.
+//   cd ./mymodule
+//   gopages
+//
+// To show usage:
+//   gopages -help
+//
 package main
