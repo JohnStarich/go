c3b5030c69b3c6b74c74b38d2b2062db1f0374c5
John Starich <johnstarich@johnstarich.com>
2021-10-01 19:44:50 -0600

    pipe: Add option to continue processing in spite of errors

--- pipe/error.go
+++ pipe/error.go
@@ -1,6 +1,10 @@
 package pipe
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 func CheckError(cond bool, err error) error {
 	if cond {
@@ -14,4 +18,44 @@ func CheckErrorf(cond bool, format string, args ...interface{}) error {
 		return fmt.Errorf(format, args...)
 	}
 	return nil
+}
+
+type Error struct {
+	errs []error
+}
+
+func (e Error) Error() string {
+	if len(e.errs) == 1 {
+		return fmt.Sprintf("pipe: %v", e.errs[0])
+	}
+	errStrs := make([]string, len(e.errs))
+	for i, err := range e.errs {
+		errStrs[i] = err.Error()
+	}
+	return fmt.Sprintf("pipe: multiple errors: %s", strings.Join(errStrs, "; "))
+}
+
+// Unwrap implements errors.Unwrap() defined interface. Returns the first error only.
+func (e Error) Unwrap() error {
+	return e.errs[0]
+}
+
+// As implements errors.As() defined interface
+func (e Error) As(target interface{}) bool {
+	for _, candidate := range e.errs {
+		if errors.As(candidate, target) {
+			return true
+		}
+	}
+	return false
+}
+
+// Is implements errors.Is() defined interface
+func (e Error) Is(target error) bool {
+	for _, candidate := range e.errs {
+		if errors.Is(candidate, target) {
+			return true
+		}
+	}
+	return false
 }
--- pipe/go.mod
+++ pipe/go.mod
@@ -1,3 +1,3 @@
 module github.com/johnstarich/go/pipe
 
-go 1.11
+go 1.13
