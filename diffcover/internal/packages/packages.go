fb2853dbd61d7afac04c87302facb6eeb972fff6
John Starich <johnstarich@johnstarich.com>
2022-06-04 15:56:25 -0500

    Add tests

--- diffcover/internal/packages/packages.go
+++ diffcover/internal/packages/packages.go
@@ -29,58 +29,36 @@ type Options struct {
 // If you would like to include more sources for the current Go build context, customize them with 'options'.
 func FilePath(fs hackpadfs.FS, workingDirectory, filePattern string, options Options) (pkgFile string, err error) {
 	memFS, err := mem.NewFS()
-	if err != nil {
-		return "", err
-	}
+	panicIfErr(err)
 	mountFS, err := mount.NewFS(memFS)
-	if err != nil {
-		return "", err
-	}
+	panicIfErr(err)
 
 	const (
 		dirPerm = 0700
 		workDir = "work"
 		tempDir = "tmp"
 	)
-	if err := memFS.Mkdir(workDir, dirPerm); err != nil {
-		return "", err
-	}
-	if err := mountFS.AddMount(workDir, fs); err != nil {
-		return "", err
-	}
+	var sErr error
+	setErr(memFS.Mkdir(workDir, dirPerm), &sErr)
+	setErr(mountFS.AddMount(workDir, fs), &sErr)
 	workingDirectory = path.Join(workDir, workingDirectory)
-	if err := memFS.Mkdir(tempDir, dirPerm); err != nil {
-		return "", err
-	}
+	setErr(memFS.Mkdir(tempDir, dirPerm), &sErr)
 	options.GoPath = makePathList([]string{tempDir}, filepath.SplitList(options.GoPath))
 
-	moduleName, moduleDir, err := getModule(mountFS, workingDirectory)
-	if err != nil {
-		return "", err
-	}
+	moduleName, moduleDir, err := findModule(mountFS, workingDirectory)
+	setErr(err, &sErr)
 	trimDir := workingDirectory
 	if moduleName != "" {
 		memFS, err := mem.NewFS()
-		if err != nil {
-			return "", err
-		}
-		err = mountFS.AddMount(tempDir, memFS)
-		if err != nil {
-			return "", err
-		}
+		panicIfErr(err)
+		setErr(mountFS.AddMount(tempDir, memFS), &sErr)
 		moduleGoPath := path.Join("src", moduleName)
-		err = memFS.MkdirAll(moduleGoPath, 0700)
-		if err != nil {
-			return "", err
-		}
+		setErr(memFS.MkdirAll(moduleGoPath, 0700), &sErr)
 		moduleFS, err := hackpadfs.Sub(mountFS, moduleDir)
 		if err != nil {
 			return "", err
 		}
-		err = mountFS.AddMount(path.Join(tempDir, moduleGoPath), moduleFS) // "symlink" original fs inside the new GOPATH-like directory
-		if err != nil {
-			return "", err
-		}
+		setErr(mountFS.AddMount(path.Join(tempDir, moduleGoPath), moduleFS), &sErr) // "symlink" original fs inside the new GOPATH-like directory
 		workDirSubPath := strings.TrimPrefix(workingDirectory, moduleDir)
 		workDirSubPath = strings.TrimPrefix(workDirSubPath, "/")
 		trimDir = path.Join(tempDir, moduleGoPath, workDirSubPath)
@@ -89,12 +67,22 @@ func FilePath(fs hackpadfs.FS, workingDirectory, filePattern string, options Opt
 	packageName, coverageFile := path.Split(filePattern)
 	ctx := newFSBuildContext(mountFS, workingDirectory, options)
 	pkg, err := ctx.Import(packageName, workingDirectory, build.FindOnly)
-	if err != nil {
-		return "", err
-	}
+	setErr(err, &sErr)
 	coverageFile = path.Join(pkg.Dir, coverageFile)
 	coverageFile = strings.TrimPrefix(coverageFile, trimDir+"/")
-	return coverageFile, nil
+	return coverageFile, sErr
+}
+
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setErr(err error, setErr *error) {
+	if err != nil && *setErr == nil {
+		*setErr = err
+	}
 }
 
 func makePathList(elems ...[]string) string {
@@ -152,24 +140,35 @@ func newFSBuildContext(fs hackpadfs.FS, workingDirectory string, options Options
 	return ctx
 }
 
-func getModule(fs hackpadfs.FS, dir string) (moduleName, moduleDir string, err error) {
+func findModule(fs hackpadfs.FS, dir string) (moduleName, moduleDir string, err error) {
 	for ; dir != "."; dir = path.Dir(dir) {
-		file := path.Join(dir, "go.mod")
-		_, err = hackpadfs.Stat(fs, file)
-		if err != nil && !errors.Is(err, hackpadfs.ErrNotExist) {
-			return "", "", err
-		}
-		if err == nil {
-			contents, err := hackpadfs.ReadFile(fs, file)
-			if err != nil {
-				return "", "", err
-			}
-			modFile, err := modfile.Parse(file, contents, nil)
-			if err != nil {
-				return "", "", err
-			}
-			return modFile.Module.Mod.Path, dir, nil
+		var ok bool
+		moduleName, moduleDir, ok, err = getModule(fs, dir)
+		if ok || err != nil {
+			return
 		}
 	}
-	return "", "", nil
+	moduleName, moduleDir, _, err = getModule(fs, dir)
+	return
+}
+
+func getModule(fs hackpadfs.FS, dir string) (moduleName, moduleDir string, ok bool, err error) {
+	file := path.Join(dir, "go.mod")
+	_, err = hackpadfs.Stat(fs, file)
+	if err != nil {
+		if errors.Is(err, hackpadfs.ErrNotExist) {
+			err = nil
+		}
+		return "", "", false, err
+	}
+
+	contents, err := hackpadfs.ReadFile(fs, file)
+	if err != nil {
+		return "", "", false, err
+	}
+	modFile, err := modfile.Parse(file, contents, nil)
+	if err != nil {
+		return "", "", false, err
+	}
+	return modFile.Module.Mod.Path, dir, true, nil
 }
--- diffcover/internal/packages/packages_test.go
+++ diffcover/internal/packages/packages_test.go
@@ -1,10 +1,12 @@
 package packages
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/johnstarich/go/diffcover/internal/testhelpers"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFilePath(t *testing.T) {
@@ -99,6 +101,85 @@ package main
 			pkgFile, err := FilePath(fs, tc.workingDirectory, tc.filePattern, Options{})
 			assert.NoError(t, err)
 			assert.Equal(t, tc.expectPath, pkgFile)
+		})
+	}
+}
+
+func TestPanicIfErr(t *testing.T) {
+	t.Parallel()
+	someErr := errors.New("some error")
+	assert.PanicsWithValue(t, someErr, func() {
+		panicIfErr(someErr)
+	})
+	assert.NotPanics(t, func() {
+		panicIfErr(nil)
+	})
+}
+
+func TestSetErr(t *testing.T) {
+	t.Parallel()
+	someError := errors.New("some error")
+	var err error
+	setErr(nil, &err)
+	assert.NoError(t, err)
+
+	setErr(someError, &err)
+	assert.Same(t, someError, err)
+}
+
+func TestFindModule(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []struct {
+		description      string
+		dir              string
+		files            map[string]string
+		expectModuleName string
+		expectModuleDir  string
+		expectErr        string
+	}{
+		{
+			description:      "root module",
+			dir:              ".",
+			files:            map[string]string{"go.mod": "module mymodule"},
+			expectModuleName: "mymodule",
+			expectModuleDir:  ".",
+		},
+		{
+			description:      "subdirectory module",
+			dir:              "foo",
+			files:            map[string]string{"foo/go.mod": "module mymodule"},
+			expectModuleName: "mymodule",
+			expectModuleDir:  "foo",
+		},
+		{
+			description: "parent directory module",
+			dir:         "foo",
+			files: map[string]string{
+				"go.mod":  "module mymodule",
+				"foo/bar": "bar",
+			},
+			expectModuleName: "mymodule",
+			expectModuleDir:  ".",
+		},
+		{
+			description: "malformed go.mod",
+			dir:         ".",
+			files:       map[string]string{"go.mod": "not a module"},
+			expectErr:   "go.mod:1: unknown directive: not",
+		},
+	} {
+		tc := tc // enable parallel sub-tests
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
+			fs := testhelpers.FSWithFiles(t, tc.files)
+			moduleName, moduleDir, err := findModule(fs, tc.dir)
+			if tc.expectErr != "" {
+				assert.EqualError(t, err, tc.expectErr)
+				return
+			}
+			require.NoError(t, err)
+			assert.Equal(t, tc.expectModuleName, moduleName)
+			assert.Equal(t, tc.expectModuleDir, moduleDir)
 		})
 	}
 }
