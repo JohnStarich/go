9b7fa234a2e3a640d00c74fee92edfbe4154d5c0
John Starich <johnstarich@johnstarich.com>
2022-10-30 19:39:13 -0500

    Extend info command to peek each file in the bin path for a goop shebang

--- goop/cmd/goop/info.go
+++ goop/cmd/goop/info.go
@@ -10,16 +10,40 @@ import (
 )
 
 func (a App) info(c *cli.Context) error {
-	dirEntries, err := hackpadfs.ReadDir(a.fs, path.Join(a.configDir, configBin))
+	binPath := path.Join(a.configDir, configBin)
+	dirEntries, err := hackpadfs.ReadDir(a.fs, binPath)
 	if err != nil && !errors.Is(err, hackpadfs.ErrNotExist) {
 		return err
 	}
 
-	fmt.Fprintln(c.App.Writer, "Installed:")
+	fmt.Fprintf(c.App.Writer, "Installed: (%s)\n", binPath)
 	for _, entry := range dirEntries {
-		if entry.Type().IsRegular() {
+		isInstalled, err := isAppExecutable(a.fs, entry.Type(), path.Join(binPath, entry.Name()))
+		if err != nil {
+			return err
+		}
+		if isInstalled {
 			fmt.Fprintln(c.App.Writer, "-", entry.Name())
 		}
 	}
 	return nil
+}
+
+func isAppExecutable(fs hackpadfs.FS, mode hackpadfs.FileMode, filePath string) (bool, error) {
+	if !mode.IsRegular() && mode&hackpadfs.ModeSymlink == 0 {
+		return false, nil
+	}
+	f, err := fs.Open(filePath)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	const expectedShebangPrefix = `#!/usr/bin/env goop `
+	shebangPrefix := make([]byte, len(expectedShebangPrefix))
+	_, err = f.Read(shebangPrefix)
+	if err != nil {
+		return false, err
+	}
+	return expectedShebangPrefix == string(shebangPrefix), nil
 }
